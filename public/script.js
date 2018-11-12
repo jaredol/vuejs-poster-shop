0bedd10540570bc977a85cac0dfb9edefb424284
Jared O <jared.oloughlin@gmail.com>
2018-11-12 15:28:13 +1100

    Initial commit

--- public/script.js
+++ public/script.js
@@ -1,1 +1,71 @@
-console.log('It works.');
+var PRICE = 9.99;
+
+new Vue({
+    el: "#app",
+    data: {
+        total: 0,
+        items: [
+            { id: 1, title: 'Item 1', price: 10 },
+            { id: 2, title: 'Item 2', price: 20 },
+            { id: 3, title: 'Item 3', price: 30 }
+        ],
+        cart: [],
+        search: ''
+    },
+    methods: {
+        onSubmit: function () {
+            this.$http
+                .get('/search/'.concat(this.search))
+                .then(function (res) {
+                    console.log(res.body[0].title);
+                });
+        },
+        addItem: function (index) {
+            this.total += PRICE;
+            var item = this.items[index];
+            var found = false;
+
+            for (var i = 0; i < this.cart.length; i++) {
+                // if its already there
+                if (this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+
+            // if not found
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: item.price
+                })
+            }
+        },
+
+        inc: function (item) {
+            item.qty++;
+            this.total += PRICE;
+        },
+
+        dec: function (item) {
+            item.qty--;
+            this.total -= PRICE;
+            if (item.qty <= 0) {
+                for (var i = 0; i < this.cart.length; i++) {
+                    if (this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                    }
+                }
+            }
+        }
+
+    },
+    filters: {
+        currency: function (price) {
+            return '$'.concat(price.toFixed(2));
+        }
+    }
+});
